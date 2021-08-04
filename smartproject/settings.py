19aef2d95ad478b2629423d36f7d71c5fdd5a2f4
KenBroTech <80349696+KenBroTech@users.noreply.github.com>
2021-08-04 14:28:58 +0100

    Update settings.py

--- smartproject/settings.py
+++ smartproject/settings.py
@@ -127,6 +127,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = (BASE_DIR / 'staticfiles')
+
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'dashboard-index'
